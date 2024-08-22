5ce8f05683e920d30eb6cd68da09ec8133bb56f4
Nicola Brisotto <nicolabrisotto@gmail.com>
2024-08-22 19:18:18 +0200

    Update run.sh
    Support both linux and Darwin
    - Fix issues with sed syntax on darwin
    - Fix issues with missing inotify on darwin

--- run.sh
+++ run.sh
@@ -2,17 +2,40 @@
 DESTINATION=$1
 PORT=$2
 CHAT=$3
+
 # clone Odoo directory
 git clone --depth=1 https://github.com/minhng92/odoo-17-docker-compose $DESTINATION
 rm -rf $DESTINATION/.git
+
 # set permission
 mkdir -p $DESTINATION/postgresql
 sudo chmod -R 777 $DESTINATION
-# config
-if grep -qF "fs.inotify.max_user_watches" /etc/sysctl.conf; then echo $(grep -F "fs.inotify.max_user_watches" /etc/sysctl.conf); else echo "fs.inotify.max_user_watches = 524288" | sudo tee -a /etc/sysctl.conf; fi
-sudo sysctl -p
-sed -i 's/10017/'$PORT'/g' $DESTINATION/docker-compose.yml
-sed -i 's/20017/'$CHAT'/g' $DESTINATION/docker-compose.yml
+
+# Check if running on macOS
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  echo "Running on macOS. Skipping inotify configuration."
+else
+  # config for Linux
+  if grep -qF "fs.inotify.max_user_watches" /etc/sysctl.conf; then
+    echo $(grep -F "fs.inotify.max_user_watches" /etc/sysctl.conf)
+  else
+    echo "fs.inotify.max_user_watches = 524288" | sudo tee -a /etc/sysctl.conf
+  fi
+  sudo sysctl -p
+fi
+
+
+# Update docker-compose configuration
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  # macOS sed syntax
+  sed -i '' 's/10017/'$PORT'/g' $DESTINATION/docker-compose.yml
+  sed -i '' 's/20017/'$CHAT'/g' $DESTINATION/docker-compose.yml
+else
+  # Linux sed syntax
+  sed -i 's/10017/'$PORT'/g' $DESTINATION/docker-compose.yml
+  sed -i 's/20017/'$CHAT'/g' $DESTINATION/docker-compose.yml
+fi
+
 # run Odoo
 docker-compose -f $DESTINATION/docker-compose.yml up -d
 
